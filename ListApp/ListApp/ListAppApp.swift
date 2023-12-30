e28b7eb89823726258c7b1ccbd1dc2b12bfdc427
Julia Popova <yuliarandomchenko@gmail.com>
2023-12-30 19:58:50 +0300

    Init project

---
+++ ListApp/ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Julia Popova on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
